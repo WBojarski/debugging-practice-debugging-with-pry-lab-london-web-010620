63226276183a3e1e0bec0af65d5ec34ccdd8e3a3
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-30 11:32:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,8 +9,8 @@ end
 
 def turtle_traits(turtles)
   turtles.each do |turtle|
-    turtle[:traits].each_value do |trait|
-      trait
+    turtle[:traits].each_value do |value|
+      value
     end
   end
   
