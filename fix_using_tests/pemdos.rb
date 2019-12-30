8bff5073f9f8e48a6229ce001f7d60cc72ea81c4
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-30 09:48:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,7 @@
+
 def snake_it_up(string)
   if string[0] == "s"
-  ten_s = 10.times do |letter|
-    "s"
-  end
-  ten_s = ten_s.to_s + string.to_s
-  ten_s
+   "ssssssssss" + string
   else
   string
   end
