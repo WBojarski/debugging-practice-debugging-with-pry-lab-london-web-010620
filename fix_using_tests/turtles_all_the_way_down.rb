460399657d0922144deecbcbe9b8c581090849b9
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-30 11:23:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -13,5 +13,22 @@ def turtle_traits(turtles)
   end
   
 end
+def turtles
+    [
+      {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
+      {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
+      {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
+    ]
+end
+
+def turtle_traits(turtles)
+  turtles.select { |key| turtles[:traits]}
+
+  
+end
+
+
+
 
 
